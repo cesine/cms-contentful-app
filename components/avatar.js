1b55987abe112c9a25e5a0da2914e7b123182374
cesine <cesine@yahoo.com>
2022-10-29 14:33:00 +0200

    Error: Image with src is missing width property

--- components/avatar.js
+++ components/avatar.js
@@ -1,6 +1,6 @@
 import ContentfulImage from './contentful-image'
 
-export default function Avatar({ name, picture }) {
+export default function Avatar({ name, picture, width = 200, height = 200 }) {
   return (
     <div className="flex items-center">
       <div className="relative w-12 h-12 mr-4">
@@ -9,6 +9,8 @@ export default function Avatar({ name, picture }) {
           layout="fill"
           className="rounded-full"
           alt={name}
+          width={width}
+          height={height}
         />
       </div>
       <div className="text-xl font-bold">{name}</div>
